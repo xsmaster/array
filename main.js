1cb3b2c4db1b4daf02b106c0521a05933c7e6656
xsmaster <165388332+xsmaster@users.noreply.github.com>
2024-05-27 15:18:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let Username = ["asad", "sherzod", "ahmad", "saida", "aziz", "botir", "samir", "suhoy"];
+console.log(Username);
+let Uzunligi = alert(`Arrayni uzunligi: ${Username.length}`);
+let Tekshirish = confirm("Siz Arrayni ichida ma'motni olib tashlamoqchimisiz");
+if (Tekshirish == true){
+    console.log(Username.pop());
+    console.log(Username);
+}
+else{
+    log(Username);
+}
+
+let a = alert("Siz Arrayni ichida ma'motni olib tashladingiz")
+console.log(a.);
